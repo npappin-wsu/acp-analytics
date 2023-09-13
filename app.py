d327214f44984a8077f68a86d75bea358e7aab59
W. Nick Pappin <nick.pappin@wsu.edu>
2023-09-13 11:40:07 -0700

    Updated style sheet a little.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dash import Dash, dcc, html
 
 file = os.path.join('data', 'waAcp.parquet')
 
-data = (
+acpData = (
     pd.read_parquet(file)
     .query("State == 'WA' and `County Name` == 'ADAMS COUNTY'")
     .sort_values(by="Data Month")
@@ -12,26 +12,42 @@ data = (
 
 # Join Max reciepients into data!!
 
-app = Dash(__name__)
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Washington ACP Adoption"
 application = app.server
 
 app.layout = html.Div(
     children=[
-        html.H1(children='Washington ACP Analytics'),
-        html.P(
-            children='Review the adoption of ACP over time in Washington'
+        html.Div(
+            children = [
+                html.Img(src='assets/WSU-EXT-lockup-horz-rgb-12in.gif', className='header-logo'),
+                html.H1(children='ACP in Washington'),
+                html.P(
+                    children='How well is the ACP adopted in Washington?'
+                ),
+            ],
+            className='header'
         ),
         dcc.Graph(
             figure={
                 'data': [
                     {
-                        'x': data['Data Month'],
-                        'y': data['Total Subscribers'],
+                        'x': acpData['Data Month'],
+                        'y': acpData['Total Subscribers'],
                         'type': 'lines',
                         'name': 'ACP Subscribers (Real data)'
                     },
                     {
-                        'x': [data['Data Month'].min(), data['Data Month'].max()],
+                        'x': [acpData['Data Month'].min(), acpData['Data Month'].max()],
                         'y': [800, 800],
                         'type': 'lines',
                         'name': 'Possible Subscribers (Fake data)',
